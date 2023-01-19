2e899ca8a4912a80efbbcaeefc89d1a4c64cdf16
Togsaaay <coolestjanith01@gmail.com>
2023-01-19 20:39:25 +0800

    Adding some designs

--- main.cpp
+++ main.cpp
@@ -44,28 +44,38 @@ int main()
 {
     int choice;
 
+    string J = "Computer Programming 1";
     string S = "Activity Compilation";
 
     Color();
 
     cout << "\t\t\t\t\t\t  ";
+    for (int i=0 ; i < J[i]; i++)
+    {
+        cout << J[i] ;
+        Sleep(30);
+    }
+
+    cout << endl;
+    cout << "\t\t\t\t\t\t   ";
     for (int i=0 ; i < S[i]; i++)
     {
         cout << S[i] ;
         Sleep(30);
     }
-    cout << "\n\n\n\t\t\t\t\t\t    [1] Activity 1";
-    cout << "\n\t\t\t\t\t\t    [2] Activity 2";
-    cout << "\n\t\t\t\t\t\t    [3] Activity 3";
-    cout << "\n\t\t\t\t\t\t    [4] Activity 4";
-    cout << "\n\t\t\t\t\t\t    [5] Activity 5";
-    cout << "\n\t\t\t\t\t\t    [6] Activity 6";
-    cout << "\n\t\t\t\t\t\t    [7] Activity 7";
-    cout << "\n\t\t\t\t\t\t    [8] Activity 8";
-    cout << "\n\t\t\t\t\t\t    [9] Activity 9";
-    cout << "\n\t\t\t\t\t\t    [0] Activity 10";
-    cout << endl << "\n\t\t\t\t\t\t    [X] To Exit";
-    cout << "\n\n\t\t\t\t\t\t Enter your choice: ";
+
+    cout << "\n\n\n\t\t\t\t\t\tPress [1] To View Activity 1";
+    cout << "\n\t\t\t\t\t\tPress [2] To View Activity 2";
+    cout << "\n\t\t\t\t\t\tPress [3] To View Activity 3";
+    cout << "\n\t\t\t\t\t\tPress [4] To View Activity 4";
+    cout << "\n\t\t\t\t\t\tPress [5] To View Activity 5";
+    cout << "\n\t\t\t\t\t\tPress [6] To View Activity 6";
+    cout << "\n\t\t\t\t\t\tPress [7] To View Activity 7";
+    cout << "\n\t\t\t\t\t\tPress [8] To View Activity 8";
+    cout << "\n\t\t\t\t\t\tPress [9] To View Activity 9";
+    cout << "\n\t\t\t\t\t\tPress [0] To View Activity 10";
+    cout << endl << "\n\t\t\t\t\t\t     Press [X] To Exit";
+    cout << "\n\n\t\t\t\t\t\t     Enter your choice: ";
     fflush(stdin);
     choice = getch();
     system("cls");
